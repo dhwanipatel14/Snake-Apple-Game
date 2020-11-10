b3350ab8f5e2115184489dab8249a364735aae44
Dhwani Patel <66276887+dhwanipatel14@users.noreply.github.com>
2020-11-09 21:39:50 -0500

    Update SnakeGamePanel.java

--- src/SnakeGamePanel.java
+++ src/SnakeGamePanel.java
@@ -17,8 +17,8 @@ import javax.swing.Timer;
  *  as a subclass of JPanel.
  */
 public class SnakeGamePanel extends JPanel implements ActionListener {
-	static final int SCREEN_WIDTH = 600;
-	static final int SCREEN_HEIGHT = 600;
+	static final int SCREEN_WIDTH = 1000;
+	static final int SCREEN_HEIGHT = 1000;
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
 	static final int DELAY = 75;
